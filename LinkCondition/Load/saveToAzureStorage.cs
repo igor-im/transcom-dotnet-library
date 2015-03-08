97b6e394ead82b3ef0df1969aa5e4504510b1bee
igor-im <igor@igor>
2015-03-08 16:46:21 -0400

    implemented azureblob, added DTO for future use

--- LinkCondition/Load/saveToAzureStorage.cs
+++ LinkCondition/Load/saveToAzureStorage.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace LinkCondition.Load
 {
-    class saveToAzureStorage
+    public static class saveToAzureStorage
     {
+        public static bool saveToAzureBlob(this string JSON, string connectionString, string storagecontainer)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(storagecontainer);
+            CloudBlockBlob blob = container.GetBlockBlobReference(utility.getTimeStamp()); // Get time stamp used for naming the blob in blob storage
+            var uploadtask = blob.UploadTextAsync(JSON);
+            uploadtask.Wait();
+            return uploadtask.IsCompleted;
+        }
+
+        public static string createStorageConnectionString(string storageAccountName, string azureStorageAccessKey)
+        {
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=" + storageAccountName +
+                                      ";AccountKey=" + azureStorageAccessKey;
+            return connectionString;
+        }
     }
 }
---
+++ LinkCondition/utility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkCondition
+{
+    public static class utility
+    {
+        public static string getTimeStamp()
+        {
+            return DateTime.Now.ToString("MMddyyyHHmmss");
+        }
+    }
+}
