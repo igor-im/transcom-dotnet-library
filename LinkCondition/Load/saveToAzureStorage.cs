cd933659f78d9ab777c4166d8741b0261ff25068
igor-im <igor@igor>
2015-03-09 04:39:32 -0400

    saveToAzureStorageQueue implemented

--- LinkCondition/Load/saveToAzureStorage.cs
+++ LinkCondition/Load/saveToAzureStorage.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace LinkCondition.Load
 {
@@ -17,6 +18,18 @@ namespace LinkCondition.Load
             CloudBlobContainer container = blobClient.GetContainerReference(storagecontainer);
             CloudBlockBlob blob = container.GetBlockBlobReference(utility.getTimeStamp()); // Get time stamp used for naming the blob in blob storage
             var uploadtask = blob.UploadTextAsync(JSON);
+            uploadtask.Wait();
+            return uploadtask.IsCompleted;
+        }
+
+        public static bool saveToAzureStorageQueue(this string JSON, string connectionString, string queuename)
+        {
+            // todo: add code to check and handle 64KB message limit for queues
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            CloudQueue queue = queueClient.GetQueueReference(queuename);
+            CloudQueueMessage message = new CloudQueueMessage(JSON);
+            var uploadtask = queue.AddMessageAsync(message);
             uploadtask.Wait();
             return uploadtask.IsCompleted;
         }
