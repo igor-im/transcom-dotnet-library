7c661c18261d245bc72d57d85b221fd9f6e6f723
igor-im <igor@igor>
2015-03-09 04:10:13 -0400

    Adding Test folder

---
+++ LinkConditionTests/Load/saveToAzureStorageTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkCondition.Load;
+using NUnit.Framework;
+namespace LinkCondition.Load.Tests
+{
+    [TestFixture()]
+    public class saveToAzureStorageTests
+    {
+        [Test()]
+        public void createStorageConnectionStringTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
