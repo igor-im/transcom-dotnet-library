49d57d1deb93153e46563c94796549feb2c5cf2a
igor-im <igor@igor>
2015-03-10 10:45:11 -0400

    added local save

--- LinkCondition/Load/saveToLocal.cs
+++ LinkCondition/Load/saveToLocal.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,16 @@ namespace LinkCondition.Load
 {
     class saveToLocal
     {
+        public static bool saveToLocalDisk(string JSON, string folderpath)
+        {
+            bool status = false;
+            using (TextWriter writer = File.CreateText(folderpath + utility.getTimeStamp() + ".json"))
+            {
+                var temp = writer.WriteAsync(JSON);
+                temp.Wait();
+                status = temp.IsCompleted;
+            }
+            return status;
+        }
     }
 }
